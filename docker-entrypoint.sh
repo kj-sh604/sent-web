28e41c18d5391a49a66f05042601c918456dadc0
kj_sh604 <43.splash@gmail.com>
2026-03-16 13:53:01 -0400

    refactor: docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ -z "$(ls -A /var/www/html/src/uploads 2>/dev/null)" ] && \
+if [ -z "$(ls -A /app/uploads 2>/dev/null)" ] && \
    [ -d /opt/uploads-seed ]; then
-    cp -r /opt/uploads-seed/. /var/www/html/src/uploads/
+    cp -r /opt/uploads-seed/. /app/uploads/
 fi
 
-chown -R www-data:www-data /var/www/html/src/uploads
+chown -R www-data:www-data /app/uploads
 
 exec "$@"
